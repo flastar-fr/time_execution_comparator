9e5fffdc6a8c0c3797fb16b0ab742c14d0a081f1
flastar-fr <hgbequet@gmail.com>
2024-10-08 21:54:28 +0200

    Remove of processes

--- time_execution_comparator/execution_comparator.py
+++ time_execution_comparator/execution_comparator.py
@@ -1,6 +1,7 @@
 import time
 import typing
-from multiprocessing import Process, Manager
+from collections.abc import Callable
+
 from easy_console_table import TwoEntryTable
 from tqdm import tqdm
 
@@ -13,74 +14,43 @@ def execute_function(func: typing.Callable):
 
 class ExecutionComparator:
     def __init__(self):
-        self.table = TwoEntryTable()
+        self.table: TwoEntryTable = TwoEntryTable()
         self.table.add_column_names("Total\nExecution\nTime", "Average\nTime", "Max\nTime\nExecution",
                                     "Min\nTime\nExecution", "Difference", "Iterations")
         self.table.title = "Time\nCalculator"
-        manager_returned_values = Manager()
-        self.return_values = manager_returned_values.dict()
-        manager_advancements = Manager()
-        self.advancements = manager_advancements.dict()
-
-    def run_test(self, return_values: dict, func: typing.Callable, iterations: int = 1):
-        total_times = []
-        function_name = func.__name__
-        for i in range(iterations):
-            self.advancements[function_name] = i
-            result = execute_function(func)
-            total_times.append(result)
-
-        # data analysis
-        total_time = sum(total_times)
-        average_time = sum(total_times) / iterations
-        max_time = max(total_times)
-        min_time = min(total_times)
-
-        return_values[function_name] = [total_time, average_time, max_time, min_time, max_time - min_time]
+        self.returned_value: dict[str: list[float]] = {}
 
     def reset(self):
-        self.table = TwoEntryTable()
+        self.table: TwoEntryTable = TwoEntryTable()
         self.table.add_column_names("Total\nExecution\nTime", "Average\nTime", "Max\nTime\nExecution",
                                     "Min\nTime\nExecution", "Difference", "Iterations")
         self.table.title = "Time\nCalculator"
-        manager_returned_values = Manager()
-        self.return_values = manager_returned_values.dict()
-        manager_advancements = Manager()
-        self.advancements = manager_advancements.dict()
+        self.returned_value: dict[str: list[float]] = {}
 
-    def compare_execution(self, *args: typing.Callable, iterations: int = 1):
-        processes = []
-        for func in args:
-            process = Process(target=self.run_test, args=[self.return_values, func, iterations])
-            processes.append(process)
-            process.start()
-
-        progress_bar = tqdm(total=iterations, desc="Overall Progress")
-
-        while True:
-            all_finished = all(not process.is_alive() for process in processes)
-
-            if all_finished:
-                break
-
-            min_advancement = 0
-            if len(self.advancements.values()) > 0:
-                min_advancement = min(self.advancements.values())
-
-            progress_bar.update(min_advancement - progress_bar.n)
-
-            time.sleep(0.1)
-
-        progress_bar.update(iterations)
+    def execute_iterations(self, func: Callable, iterations: int):
+        progress_bar = tqdm(total=iterations, desc=f"Overall Progress for {func.__name__}")
+        function_values = []
+        for _ in range(iterations):
+            function_values.append(execute_function(func))
+            progress_bar.update(1)
+        max_value = max(function_values)
+        min_value = min(function_values)
+        self.returned_value[func.__name__] = [
+            sum(function_values),  # total time
+            sum(function_values) / iterations,  # average time
+            max_value,  # max time
+            min_value,  # min time
+            max_value - min_value  # difference
+        ]
         progress_bar.close()
 
-        # prevent bugs
-        for process in processes:
-            process.join()
+    def compare_execution(self, *args: typing.Callable, iterations: int = 1):
+        for func in args:
+            self.execute_iterations(func, iterations)
 
-        for k, v in self.return_values.items():
+        for k, v in self.returned_value.items():
             self.table.add_line_names(k)
             v = [f"{val: .4E}" if isinstance(val, float) else val for val in v]
-            self.table.add_values(k, v + [iterations])
+            self.table.add_values(k, v + [f"{iterations: _}"])
 
         return str(self.table)
